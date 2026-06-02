99242119d6808650efe4b4e3759266b87806c5bf
Sparsh Suryan <23103378@mail.jiit.ac.in>
2026-06-02 10:22:12 +0530

    Configure Prisma client and database connectivity

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = 3000;
 
+const prisma = require("./src/config/prisma");
+
 // Root Route
 app.get("/", (req, res) => {
     res.send("Inventory Management Software Backend Running");
@@ -28,6 +30,24 @@ app.get("/api/products", (req, res) => {
         }
     ]);
 });
+
+app.get("/api/test-db", async (req, res) => {
+    try {
+      const categories = await prisma.category.findMany();
+  
+      res.json({
+        success: true,
+        data: categories,
+      });
+    } catch (error) {
+      console.error(error);
+  
+      res.status(500).json({
+        success: false,
+        message: "Database connection failed",
+      });
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
